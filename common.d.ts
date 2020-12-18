91185bc440657870b3084d840a47704ac79d3537
pandajiny <astic1764@gmail.com>
2020-12-18 19:35:55 +0900

    update types

--- common.d.ts
+++ common.d.ts
@@ -1,13 +1,6 @@
-interface HttpsResponse<T> {
-  ok: boolean;
-  error_message?: string;
-  result?: T;
-}
-
 interface ActionResult {
   ok: boolean;
   message: string;
 }
 
-type DatabaseName = "places_schema";
 declare module "*.png";
